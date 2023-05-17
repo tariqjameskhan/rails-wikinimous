f25a66356ad784ae5ae64be273e720ffecb3d4a3
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-05-17 12:20:53 +0100

    chall

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'creating 10 articles'
+
+10.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Quote.famous_last_words
+  )
+end
+
+puts 'Finished writing'
